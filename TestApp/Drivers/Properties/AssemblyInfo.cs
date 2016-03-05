b67f47cac2bbbbf30251210a5e229b522d8a6165
tkopacz <tkopacz@tomaszkopacz.com>
2016-03-05 22:32:18 +0100

    work

---
+++ TestApp/Drivers/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GHIElectronics.UWP.LowLevelDrivers")]
+[assembly: AssemblyDescription("Low level IC drivers for UWP.")]
+[assembly: AssemblyCompany("GHI Electronics, LLC")]
+[assembly: AssemblyCopyright("Copyright © GHI Electronics, LLC 2016")]
+[assembly: AssemblyVersion("0.0.9.0")]
+[assembly: AssemblyFileVersion("0.0.9.0")]
+[assembly: ComVisible(false)]
